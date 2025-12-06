9e75cc51f2dd84ea0d2a24058721a1f5c12dfb77
DevOps Academy <devops.academy@example.com>
2025-12-06 16:36:28 -0600

    feat(ci): Ejercicio 1.5: Incorporación de actions pre-construidas

---
+++ hello.py
@@ -0,0 +1,7 @@
+# hello.py
+import sys
+import os
+
+print("¡Hola desde GitHub Actions! 🚀")
+print(f"Versión de Python utilizada: {sys.version.split()[0]}")
+print(f"El directorio actual contiene {len(os.listdir('.'))} archivos/directorios.")
